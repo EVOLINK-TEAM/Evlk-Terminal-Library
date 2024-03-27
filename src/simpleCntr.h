6813fe11ee84d48ca2a7b68f4535300ed175d3d9
AEva <crystar.z@outlook.com>
2024-03-28 01:08:53 +0800

    ✨ feat(simpleCntr): add resize func
    1.add resize func
    2.modify insert/remove logic
    3.size() to get a num

--- src/simpleCntr.h
+++ src/simpleCntr.h
@@ -1,12 +1,13 @@
 #ifndef _SIMPLECNTR_
 #define _SIMPLECNTR_
+#include "string.h"
 
 template <class T>
 class simpleCntr
 {
 private:
-    T *const Begin;
-    T *const Size;
+    T *Begin;
+    T *Size;
     T *End;
 
 public:
@@ -31,26 +32,61 @@ public:
 
         return Begin <= first && first <= secend && secend < End;
     }
+    bool resize(size_t num)
+    {
+        size_t s = Size - Begin;
+        size_t e = End - Begin > num ? num : End - Begin;
+        if (num == s)
+            return true;
+        T *New = new T[s + 1];
+        memcpy(New, Begin, (num > s ? s : num) * sizeof(T));
+        memset(Begin, 0, sizeof(T) * (s + 1));
+        delete[] Begin;
+        Begin = New;
+        Size = Begin + num;
+        End = Begin + e;
+        return true;
+    };
+    bool resize(size_t num, const T &fill)
+    {
+        bool s = resize(num);
+        if (!s)
+            return s;
+        for (T *i = End; i <= Size; i++)
+            *i = fill;
+        return true;
+    }
     const T *insert(const T *pos, size_t num, const T &fill)
+    {
+        const T *p = insert(pos, num);
+        if (!p)
+            return p;
+        T *Pos = const_cast<T *>(pos);
+        for (T *i = Pos; i <= pos + num - 1; i++)
+            *i = fill;
+        return p;
+    };
+    const T *insert(const T *pos, size_t num)
     {
         if (!num)
             return pos;
         if (Begin > pos || pos > End || End + num > Size)
             return nullptr;
         T *Pos = const_cast<T *>(pos);
+        memmove(Pos + num, pos, sizeof(T) * (End - pos));
         End += num;
-        for (T *i = End - 1; i > pos + num - 1; i--)
-            *i = *(i - num);
-        for (T *i = Pos; i <= pos + num - 1; i++)
-            *i = fill;
         return pos;
     };
-    const T *insert(const T *pos, size_t num)
-    {
-        T f;
-        return insert(pos, num, f);
-    };
     const T *remove(const T *pos, size_t num, const T &fill, bool des = false)
+    {
+        const T *p = remove(pos, num, des);
+        if (!p)
+            return p;
+        for (T *i = End; i < End + num; i++)
+            *i = fill;
+        return p;
+    };
+    const T *remove(const T *pos, size_t num, bool des = false)
     {
         if (!num)
             return pos;
@@ -63,19 +99,9 @@ public:
                 i->~T();
                 *i = T();
             }
-        for (T *i = Pos; i < End - num; i++)
-        {
-            *i = *(i + num);
-        }
-        for (T *i = End - num; i < End; i++)
-            *i = fill;
+        memmove(Pos, pos + num, sizeof(T) * (End - pos));
         End -= num;
         return pos;
-    };
-    const T *remove(const T *pos, size_t num, bool des = false)
-    {
-        T f;
-        return remove(pos, num, f, des);
     };
     const T *replace(const T *pos, size_t num, const T &fill, bool des = false)
     {
@@ -103,19 +129,19 @@ public:
             return *(Begin + p);
         return *Begin;
     };
+    size_t size()
+    {
+        return Size - Begin;
+    }
     const T *begin()
     {
         return this->Begin;
-    };
-    const T *size()
-    {
-        return this->Size;
-    };
+    }
     const T *end()
     {
         return this->End;
     };
-    simpleCntr(size_t size, T fill)
+    simpleCntr(size_t size, const T &fill)
         : simpleCntr(size)
     {
         for (T *i = Begin; i <= Size; i++)
